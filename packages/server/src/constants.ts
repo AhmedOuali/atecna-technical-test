2f9b1b5d463cd9e89db01009ec342b1bab43b749
WALI Ahmed <ahmed.wali@soprahr.com>
2021-03-23 15:37:29 +0100

    server: add constants file (contains source_games)

---
+++ packages/server/src/constants.ts
@@ -0,0 +1,9 @@
+export default {
+  source_games: [
+    743, // /directory/game/Chess
+    138585, // /directory/game/Hearthstone
+    30921, // /directory/game/Rocket%20League
+    29595  // /game/Dota%202
+  ]
+}
+
