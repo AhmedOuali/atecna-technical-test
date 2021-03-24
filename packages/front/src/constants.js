dcc777c8ae6905bfecad970b3d420a06342ec699
WALI Ahmed <ahmed.wali@soprahr.com>
2021-03-24 03:12:46 +0100

    front: prepare deployement requirements

--- packages/front/src/constants.js
+++ packages/front/src/constants.js
@@ -1,4 +1,13 @@
-module.exports = {
+module.exports = process.env.NODE_ENV === "production" ? {
+  LINE_CHART_POINT_DENSITY: 100,
+  WS_URL: "https://test-api-23032021.herokuapp.com",
+  SOURCE_GAMES: [
+    743, // /directory/game/Chess
+    138585, // /directory/game/Hearthstone
+    30921, // /directory/game/Rocket%20League
+    29595  // /game/Dota%202
+  ]
+} : {
   LINE_CHART_POINT_DENSITY: 100,
   WS_URL: "http://localhost:3001",
   SOURCE_GAMES: [
