e5f93ee2cfb2ac8b0293523b33323d74e4a91864
WALI Ahmed <ahmed.wali@soprahr.com>
2021-03-24 03:17:46 +0100

    front: prepare deployement requirements

--- packages/front/src/constants.js
+++ packages/front/src/constants.js
@@ -1,6 +1,6 @@
 module.exports = process.env.NODE_ENV === "production" ? {
   LINE_CHART_POINT_DENSITY: 100,
-  WS_URL: "https://test-api-23032021.herokuapp.com",
+  WS_URL: "https://atecna-api-ahmed-ouali.herokuapp.com",
   SOURCE_GAMES: [
     743, // /directory/game/Chess
     138585, // /directory/game/Hearthstone
